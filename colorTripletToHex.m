bd53b638b5a795310508f51377ded8871a40c44b
liorkirsch <lior@ctx02.lnx.biu.ac.il>
2013-10-20 18:31:06 +0300

    added color to hex conversion

---
+++ colorTripletToHex.m
@@ -0,0 +1,20 @@
+function hex = colorTripletToHex(colorArray)
+% example turn the hot colorscale to hex
+% a = hot;
+% hex_hot = colorTripletToHex(a);
+
+    numElements = size(colorArray,1);
+    hex = cell(numElements,1);
+
+    for i =1:numElements
+        hex{i} = rgb2hex( colorArray(i,:) );
+    end
+
+end
+function hex = rgb2hex( rgb)
+
+    rgb = rgb*255;
+    hex1 = dec2hex(floor(rgb/16));
+    hex2 = dec2hex(mod(rgb,16)*16);
+    hex = [hex1(1),hex2(1),hex1(2),hex2(2),hex1(3),hex2(3)];
+end
