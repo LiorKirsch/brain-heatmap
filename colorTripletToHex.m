82bd106cd41b7fcb53453ab7b0c2528504c6258b
liorkirsch <lior@ctx02.lnx.biu.ac.il>
2013-10-20 18:35:14 +0300

    added color to hex conversion

--- colorTripletToHex.m
+++ colorTripletToHex.m
@@ -3,9 +3,11 @@ function hex = colorTripletToHex(colorArray)
 % a = hot;
 % hex_hot = colorTripletToHex(a);
 %
-% colorArray should be N x 3 array (RBG) - where each element is between
+% colorArray should be N x 3 array (RGB) - where each element is between
 % zero and one.
 
+    assert( size(colorArray,2) == 3, 'colorArray should be N x 3 array (RGB)');
+    
     numElements = size(colorArray,1);
     hex = cell(numElements,1);
 
