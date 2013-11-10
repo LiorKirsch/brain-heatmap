723d41fe32f904326e37608fbe5938f2c8aa0e2c
liorkirsch <lior@lior.(none)>
2013-11-10 19:26:36 +0200

    added atlas.json

--- brainmaps.js
+++ brainmaps.js
@@ -144,7 +144,8 @@
 			window.location.replace("?spreadsheetkey=" + spreadsheetkey+ "&location="  +location + "&ontology=" + ontology);
 		}
 
-		function encode_as_img_and_link(){
+		function encode_as_img_and_link(atlasData){
+			var atlasName = atlasData["atlas"];
 			var location = getURLParameter('location');
 			 // Add some critical information
 			 $("svg").attr({ version: '1.1' , xmlns:"http://www.w3.org/2000/svg"});
@@ -153,5 +154,5 @@
 			 var b64 = Base64.encode(svg); // or use btoa if supported
 
 			 // Works in Firefox 3.6 and Webit and possibly any browser which supports the data-uri
-			 $("#buttonsAndStuff").append($("<a href-lang='image/svg+xml' href='data:image/svg+xml;base64,\n"+b64+"' title='file.svg' download='image" + location + ".svg'>Download</a>"));
+			 $("#buttonsAndStuff").append($("<a href-lang='image/svg+xml' href='data:image/svg+xml;base64,\n"+b64+"' title='file.svg' download='" + atlasName + "-" + location + ".svg'>Download</a>"));
 		}
